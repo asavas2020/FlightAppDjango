311b4b57c6369b49b2792416cf5fdefa411b2093
Gokhan <gknsvsdata@gmail.com>
2022-09-30 14:13:39 +0300

    ch

--- flight/serializers.py
+++ flight/serializers.py
@@ -16,8 +16,38 @@ class FlightSerializer(serializers.ModelSerializer):
             "etd"
         )
 
+class PassengerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = "__all__"
+
+
 class ReservationSerializer(serializers.ModelSerializer):
-    
+
+    Passenger = PassengerSerializer(many = True, required = True)
+    flight = serializers.StringRelatedField() # default read_only true
+    flight_id = serializers.IntegerField(write_only = True)
+    user = serializers.StringRelatedField() # default read_only true
+    user_id = serializers.IntegerField(write_only = True, required = False)
+
     class Meta:
         model = Reservation
-        fields = "__all__"
+        fields = (
+            "id"
+            "flight" # get
+            "flight_id" # post
+            "user" # get
+            "user_id" # post
+            "Passenger"
+        )
+
+    def create(self, validated_data):
+        passenger_data = validated_data.pop("Passenger")
+        validated_data["user_id"] = self.context["request"].user.id 
+        reservation = Reservation.objects.create(**validated_data)
+
+        for passenger in passenger_data:
+            pas = Passenger.objects.create(**passenger)
+            reservation.passenger.add(pas)
+        reservation.save()
+        return reservation
